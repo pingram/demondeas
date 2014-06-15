717b9731370b33ea366139c5735b454a602822ac
Philip Ingram <philip.d.ingram@gmail.com>
2014-06-15 14:01:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demondeas::Application.config.secret_key_base = '839b016d8b501aa2e07a4f39c85911c245e56ef99ecbad90ac4bb6efeb031c5a01b679bec56aefb3f55b8f614e735bbd6624d59ba044a98a28310e03ed083874'
